1e3ed9772b69c85b513484789c871b55ffa7fa57
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2024-01-23 02:31:37 +0100

    awda

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import HomeExtras from "../components/HomeExtras";
 import HomeExtras2 from "../components/HomeExtras2";
 import Support from "../components/Support";
 import Freetrial from "../components/Freetrial";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
     return(
@@ -15,6 +16,7 @@ const HomePage = () => {
             <HomeExtras2/>
             <Support/>
             <Freetrial/>
+            <Footer/>
         </div>
     );
 }
